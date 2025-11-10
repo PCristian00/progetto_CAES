4bc8c9d2b1f7834cc393cf4e1b45d74bab3011e3
Cristian Pietroniro <95343580+PCristian00@users.noreply.github.com>
2025-11-10 17:53:45 +0100

    pulizia commenti

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -35,10 +35,6 @@ void SubSynthAudioProcessorEditor::paint(juce::Graphics& g)
 {
 	// (Our component is opaque, so we must completely fill the background with a solid colour)
 	g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
-
-	/*g.setColour(juce::Colours::white);
-	g.setFont(juce::FontOptions(15.0f));*/
-	// g.drawFittedText("Sub Synth", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void SubSynthAudioProcessorEditor::resized()
