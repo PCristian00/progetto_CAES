fbbf8f258d7ced5656df893fd739660df1c92308
Cristian Pietroniro <95343580+PCristian00@users.noreply.github.com>
2025-11-14 17:47:14 +0100

    FilterComponent (GUI)

---
+++ Source/UI/FilterComponent.h
@@ -0,0 +1,45 @@
+/*
+  ==============================================================================
+
+    FilterComponent.h
+    Created: 14 Nov 2025 4:32:15pm
+    Author:  crist
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "Utils.h"
+using namespace utils;
+
+//==============================================================================
+/*
+*/
+class FilterComponent  : public juce::Component
+{
+public:
+    FilterComponent(juce::AudioProcessorValueTreeState& apvts, juce::String filterTyperSelectorId);
+    ~FilterComponent() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+
+    juce::ComboBox filterTypeSelector{"Filter Type"};
+    std::unique_ptr<ComboBoxAttachment> filterTypeSelectorAttachment;
+
+    juce::Slider filterCutOffSlider;
+    juce::Slider filterResonanceSlider;
+
+    std::unique_ptr<SliderAttachment> filterCutOffSliderAttachment;
+    juce::Label filterFreqLabel{ "Filter Cutoff", "Filter Cutoff" };
+
+    std::unique_ptr<SliderAttachment> filterResonanceSliderAttachment;
+    juce::Label filterResonanceLabel{ "Filter Resonance", "Filter Resonance" };
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FilterComponent)
+};
