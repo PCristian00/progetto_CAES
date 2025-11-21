df2690d7641634714a8ddcd3055b226916e991a7
Cristian Pietroniro <95343580+PCristian00@users.noreply.github.com>
2025-11-21 17:30:02 +0100

    pulizia codice

--- Source/UI/PresetPanel.h
+++ Source/UI/PresetPanel.h
@@ -24,18 +24,13 @@ namespace Gui
 			configureButton(previousPresetButton, "<");
 			configureButton(nextPresetButton, ">");
 
-			// Pulsanti delete window
-			configureButton(confirmButton, "si");
-			configureButton(returnButton, "no");
-
-			// Configure presetList ComboBox here
+			// Configurazione presetList ComboBox
 			presetList.setTextWhenNothingSelected("Select Preset");
 			presetList.setMouseCursor(juce::MouseCursor::PointingHandCursor);
 			addAndMakeVisible(presetList);
 			presetList.addListener(this);
 
 			loadPresetList();
-
 		}
 
 		~PresetPanel() {
@@ -43,21 +38,13 @@ namespace Gui
 			deleteButton.removeListener(this);
 			previousPresetButton.removeListener(this);
 			nextPresetButton.removeListener(this);
-
 			presetList.removeListener(this);
 		}
-
-		// void paint(juce::Graphics& g) override;
 
 		void resized() override {
 
 			const auto container = getLocalBounds().reduced(4);
-			// const auto container = utils::getBoundsWithPadding(this, 4);
 			auto bounds = container;
-
-			// juce::Rectangle<int>
-
-			// auto buttonSize = (bounds.removeFromLeft(container.proportionOfWidth(0.2f)).reduced(4));
 
 			// Rendere migliori queste funzioni
 
@@ -66,20 +53,9 @@ namespace Gui
 			presetList.setBounds(bounds.removeFromLeft(container.proportionOfWidth(0.4f)).reduced(4));
 			setButtonBounds(nextPresetButton, bounds.removeFromLeft(container.proportionOfWidth(0.1f)).reduced(4));
 			setButtonBounds(deleteButton, bounds.reduced(4));
-			// Set bounds for presetList ComboBox
-
-			// FORSE RIUSARE quella di Utils?
-			/*auto comboBoxWidth = static_cast<int>(container.proportionOfWidth(0.2f));
-			auto comboBoxBounds = container.removeFromLeft(comboBoxWidth).reduced(4);
-			presetList.setBounds(comboBoxBounds);*/
-
-
-
-
 		}
-	private:
 
-		// FORSE IN FUTURO SERVIRANNO ANCHE ALTROVE, SPOSTARE IN UTILS?
+	private:
 
 		void loadPresetList() {
 
@@ -92,52 +68,23 @@ namespace Gui
 		}
 
 		void buttonClicked(juce::Button* button) override {
+
 			if (button == &saveButton) {
-				// Handle save button click
 				fileChooser = std::make_unique<juce::FileChooser>("Save Preset", Service::PresetManager::defaultDirectory, "*." + Service::PresetManager::extension);
 				fileChooser->launchAsync(juce::FileBrowserComponent::saveMode, [&](const juce::FileChooser& chooser) {
 					auto file = chooser.getResult();
 					auto presetName = file.getFileNameWithoutExtension();
 					presetManager.savePreset(presetName);
 					loadPresetList();
-
-					/*if (file != juce::File()) {
-						auto presetName = file.getFileNameWithoutExtension();
-						presetManager.savePreset(presetName);
-					}*/
 					});
 			}
 			else if (button == &deleteButton)
 			{
-				const auto currentPresetName = presetManager.getCurrentPreset();
 
-				if (currentPresetName.isEmpty())
+				if (presetManager.getCurrentPreset().isEmpty())
 					return;
 
-
-
 				showDeleteWindow();
-
-				//// Comportamento pulsanti ok/cancel
-				//const auto callback = juce::ModalCallbackFunction::create([this, currentPresetName](int result) {
-				//	if (result == 0) {
-
-				//	}// result == 0 means you click Cancel
-				//	if (result == 1) {
-				//		presetManager.deletePreset(currentPresetName);
-				//		loadPresetList();
-				//	}// result == 1 means you click OK
-				//	});
-
-				//juce::AlertWindow::showOkCancelBox(
-				//	juce::AlertWindow::WarningIcon,                       // Icona
-				//	"Cancella Preset",                                      // Titolo finestra
-				//	"Cancellare preset \"" + currentPresetName + "\"?", // Messaggio
-				//	"Sì",                                                // Testo pulsante OK
-				//	"No",                                                 // Testo pulsante Cancel
-				//	this,												// Component associato (per modalità)
-				//	callback 										// Nessun callback: chiamata sincrona, ritorna subito il bool
-				//);
 			}
 			else if (button == &previousPresetButton) {
 				presetList.setSelectedItemIndex(presetManager.loadPreviousPreset(), juce::dontSendNotification);
@@ -166,11 +113,11 @@ namespace Gui
 			button.setBounds(size);
 		}
 
-		// Creare componente esternO!!!
 		void showDeleteWindow() {
 
 			juce::String message = "Cancellare il preset '" + presetManager.getCurrentPreset() + "' ? ";
 
+			// Funzione eseguita alla conferma della cancellazione
 			std::function <void()> onAccept = [this]() {
 				presetManager.deletePreset(presetManager.getCurrentPreset());
 				loadPresetList();
@@ -186,13 +133,10 @@ namespace Gui
 
 		Service::PresetManager& presetManager;
 		std::unique_ptr<DialogBox> deleteDialog;
-		juce::TextButton saveButton, deleteButton, previousPresetButton, nextPresetButton, confirmButton, returnButton;
+		juce::TextButton saveButton, deleteButton, previousPresetButton, nextPresetButton;
 		juce::ComboBox presetList;
 		std::unique_ptr<juce::FileChooser> fileChooser;
 
 		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PresetPanel)
 	};
-
-
-
 } // namespace Gui
