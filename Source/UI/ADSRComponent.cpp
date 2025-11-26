ed31cf29236c7671b557d31a0497f5bda9edb4ee
Cristian Pietroniro <95343580+PCristian00@users.noreply.github.com>
2025-11-26 11:17:31 +0100

    conversione Slider (ADSRComponent)

--- Source/UI/ADSRComponent.cpp
+++ Source/UI/ADSRComponent.cpp
@@ -12,41 +12,41 @@
 #include "ADSRComponent.h"
 #include "Utils.h"
 
-//==============================================================================
-ADSRComponent::ADSRComponent(juce::String name, juce::AudioProcessorValueTreeState& apvts, juce::String attackId, juce::String decayId, juce::String sustainId, juce::String releaseId)
+ADSRComponent::ADSRComponent(juce::String name,
+	juce::AudioProcessorValueTreeState& apvts,
+	juce::String attackId,
+	juce::String decayId,
+	juce::String sustainId,
+	juce::String releaseId)
+	: componentName(name)
+	, attackLS("Attack", apvts, attackId, *this, juce::Slider::LinearBarVertical, true)
+	, decayLS("Decay", apvts, decayId, *this, juce::Slider::LinearBarVertical, true)
+	, sustainLS("Sustain", apvts, sustainId, *this, juce::Slider::LinearBarVertical, true)
+	, releaseLS("Release", apvts, releaseId, *this, juce::Slider::LinearBarVertical, true)
 {
-	componentName = name;
-
-	utils::setSliderParams(attackSlider, attackSliderAttachment, apvts, attackId, attackLabel, this);
-	utils::setSliderParams(decaySlider, decaySliderAttachment, apvts, decayId, decayLabel, this);
-	utils::setSliderParams(sustainSlider, sustainSliderAttachment, apvts, sustainId, sustainLabel, this);
-	utils::setSliderParams(releaseSlider, releaseSliderAttachment, apvts, releaseId, releaseLabel, this);
 }
-
-ADSRComponent::~ADSRComponent() {}
 
 void ADSRComponent::paint(juce::Graphics& g)
 {
-
-
 	g.fillAll(juce::Colours::black);
 	utils::drawBorders(g, this, juce::Colours::white, componentName);
-
 }
 
 void ADSRComponent::resized()
 {
 	const int numSliders = 4;
+	const auto padded = utils::getBoundsWithPadding(this);
+	const int sliderWidth = padded.getWidth() / numSliders - utils::padding;
+	const int sliderHeight = padded.getHeight() - (utils::Ystart - utils::padding);
 
-	/*const int sliderXstart = utils::padding + 5;
-	const int sliderYstart = utils::padding * 4;*/
+	int x = utils::Xstart;
+	const int y = utils::Ystart;
 
-	const int sliderWidth = utils::getBoundsWithPadding(this).getWidth() / numSliders - utils::padding;
-	const int sliderHeight = utils::getBoundsWithPadding(this).getHeight() - (utils::Ystart);
-
-
-	utils::setSliderBounds(attackSlider, utils::Xstart, utils::Ystart, sliderWidth, sliderHeight, attackLabel);
-	utils::setSliderBounds(decaySlider, attackSlider.getRight() + utils::padding, utils::Ystart, sliderWidth, sliderHeight, decayLabel);
-	utils::setSliderBounds(sustainSlider, decaySlider.getRight() + utils::padding, utils::Ystart, sliderWidth, sliderHeight, sustainLabel);
-	utils::setSliderBounds(releaseSlider, sustainSlider.getRight() + utils::padding, utils::Ystart, sliderWidth, sliderHeight, releaseLabel);
+	attackLS.setBounds(x, y, sliderWidth, sliderHeight);
+	x += sliderWidth + utils::padding;
+	decayLS.setBounds(x, y, sliderWidth, sliderHeight);
+	x += sliderWidth + utils::padding;
+	sustainLS.setBounds(x, y, sliderWidth, sliderHeight);
+	x += sliderWidth + utils::padding;
+	releaseLS.setBounds(x, y, sliderWidth, sliderHeight);
 }
