6040bed5b91df558297e5d2b4c87880635abe345
Cristian Pietroniro <95343580+PCristian00@users.noreply.github.com>
2025-12-04 18:01:29 +0100

    commenti

--- Source/Data/LimiterData.cpp
+++ Source/Data/LimiterData.cpp
@@ -1,40 +1,44 @@
 /*
   ==============================================================================
 
-    LimiterData.cpp
-    Created: 4 Dec 2025 5:35:38pm
-    Author:  crist
+	LimiterData.cpp
+	Created: 4 Dec 2025 5:35:38pm
+	Author:  crist
 
   ==============================================================================
 */
 
 #include "LimiterData.h"
+
+/*Prepara il limiter per la riproduzione.*/
 void LimiterData::prepareToPlay(double sampleRate, int samplesPerBlock, int numChannels)
 {
-    juce::dsp::ProcessSpec spec;
-    spec.sampleRate = sampleRate;
-    spec.maximumBlockSize = static_cast<juce::uint32>(samplesPerBlock);
-    spec.numChannels = static_cast<juce::uint32>(numChannels);
+	juce::dsp::ProcessSpec spec;
+	spec.sampleRate = sampleRate;
+	spec.maximumBlockSize = static_cast<juce::uint32>(samplesPerBlock);
+	spec.numChannels = static_cast<juce::uint32>(numChannels);
 
-    limiter.reset();
-    limiter.prepare(spec);
+	limiter.reset();
+	limiter.prepare(spec);
 
-    // default values
-    limiter.setThreshold(-0.5f); // dB
-    limiter.setRelease(50.0f);   // ms
+	// default values
+	limiter.setThreshold(-0.5f); // dB
+	limiter.setRelease(50.0f);   // ms
 
-    isPrepared = true;
+	isPrepared = true;
 }
 
+/*Resetta il limiter.*/
 void LimiterData::reset()
 {
-    limiter.reset();
+	limiter.reset();
 }
 
+/*Processa il buffer audio con il limiter.*/
 void LimiterData::process(juce::AudioBuffer<float>& buffer)
 {
-    jassert(isPrepared);
-    juce::dsp::AudioBlock<float> block(buffer);
-    juce::dsp::ProcessContextReplacing<float> ctx(block);
-    limiter.process(ctx);
+	jassert(isPrepared);
+	juce::dsp::AudioBlock<float> block(buffer);
+	juce::dsp::ProcessContextReplacing<float> ctx(block);
+	limiter.process(ctx);
 }
